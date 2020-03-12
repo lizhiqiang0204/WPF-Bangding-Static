1502e3a9802721e361f698e577a003371ee39a76
unknown <18518092079@163.com>
2020-03-12 14:46:54 +0800

    WPF Bangding Static

---
+++ 绑定静态字段/ClassA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace 绑定静态字段
+{
+    public class ClassA
+    {
+        private static string label = "新建类A的标签";
+        public static string myLabel
+        {
+            get { return label; }
+            set
+            {
+                label = value;
+                StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(myLabel)));//异步更新myLabel属性
+            }
+        }
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;//静态事件处理属性更改
+    }
+}
---
+++ 绑定静态字段/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace 绑定静态字段
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonA_Click(object sender, RoutedEventArgs e)
+        {
+            ClassA.myLabel = "ButtonA的事件";
+        }
+    }
+}
